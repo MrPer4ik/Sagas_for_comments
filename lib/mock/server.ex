87b8f85e265687f2f676e42ec9b0316de17dc681
Alexander Khomych <arkhomych.lit@gmail.com>
2019-12-08 17:48:49 +0200

    Added server's callbacks

--- lib/mock/server.ex
+++ lib/mock/server.ex
@@ -18,5 +18,30 @@ defmodule Mock.Server do
     def get_last_response(pid) do
       GenServer.call(pid, :get_last_response)
     end
+
+    # CALLBACKS
+
+    def init(responses) do
+        {:ok, %{last_response: :not_called, responses: responses}}
+    end
+
+    def handle_call({:request, type}, _from, state) do
+        case state[:responses][type] do
+        [] ->
+            {:stop, :cannot_handle_more_requests, state}
+
+        [response | responses] ->
+            new_state =
+            state
+            |> put_in([:last_response], response)
+            |> put_in([:responses, type], responses)
+
+            {:reply, response, new_state}
+        end
+    end
+
+    def handle_call(:get_last_response, _from, state) do
+        {:reply, state[:last_response], state}
+    end
 end
   
